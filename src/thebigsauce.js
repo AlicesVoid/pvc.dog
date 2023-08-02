c8c984cd617f7c4427cff7897d7aa78eb996e618
AlicesVoid <hypnocarrot@csu.fullerton.edu>
2023-08-01 18:44:25 -0700

    everything operates on one page now yayyyy

---
+++ src/thebigsauce.js
@@ -0,0 +1,113 @@
+/*
+function WheelSpin() {
+    const element = document.getElementById("LootWheel");
+    let angle = 0;
+    const rotationDuration = 5000; // Set the rotation duration in milliseconds
+    const interval = 10; // Set the interval for smoothness (smaller values will make it smoother)
+
+    const startTime = Date.now();
+
+    const rotateInterval = setInterval(() => {
+        const currentTime = Date.now();
+        const elapsedTime = currentTime - startTime;
+
+        if (elapsedTime >= rotationDuration) {
+            clearInterval(rotateInterval);
+        } else {
+            const decayFactor = 0.01; // Adjust the decay factor for the rate of decay
+            const decayedSpeed = 1 - Math.exp(-decayFactor * elapsedTime / rotationDuration);
+            angle -= decayedSpeed; // Adjust the rotation speed using the decayedSpeed
+            element.style.transform = `rotate(${angle}deg)`;
+        }
+    }, interval);
+}
+
+function toggleVisibility( nowVisibleName, newTitle) {
+    const nowHidden = document.getElementById(nowHiddenName);
+    const nowVisible = document.getElementById(nowVisibleName);
+    const titleElement = document.querySelector("title");
+    const oldTitle = titleElement.te
+
+    if (nowHidden.style.display === "none") {
+        nowHidden.style.display = "block";
+        nowVisible.style.display = "none";
+        if (titleElement.textContent === newTitle) {
+            titleElement.textContent = oldTitle;
+        }
+    } else {
+        nowHidden.style.display = "none";
+        nowVisible.style.display = "block";
+        if (titleElement.textContent === oldTitle) {
+            titleElement.textContent = newTitle;
+        }
+    }
+}
+*/
+
+function toggleVisibility(nowVisibleName, newTitle) {
+    const nowVisible = document.getElementById(nowVisibleName);
+    const titleElement = document.querySelector("title");
+
+    // If "nowVisibleName" is already visible and title is already set to "newTitle", do nothing
+    if (nowVisible.style.display === "block" && titleElement.textContent === newTitle) {
+        return;
+    }
+
+    // Loop through all elements to find the currently visible element
+    let nowHidden;
+    const allElements = document.getElementsByTagName("*");
+    for (let i = 0; i < allElements.length; i++) {
+        const element = allElements[i];
+        if (element.style.display === "block") {
+            nowHidden = element;
+            break;
+        }
+    }
+
+    if (nowHidden && nowHidden !== nowVisible) {
+        nowHidden.style.display = "none";
+        nowVisible.style.display = "block";
+        titleElement.textContent = newTitle;
+    }
+}
+
+//runs the Links page
+function setupToggleLinksListener() {
+    const showLinksElement = document.getElementById("showLinks");
+
+    // Adding a click event listener to the "showLinks" element
+    showLinksElement.addEventListener("click", function () {
+        event.preventDefault(); // Prevent the default link behavior (preventing page reload)
+        toggleVisibility("links_pg", "Links | 0125.dog");
+    });
+}
+
+//runs the Home page
+function setupToggleIndexListener() {
+    const showLinksElement = document.getElementById("showIndex");
+
+    // Adding a click event listener to the "showLinks" element
+    showLinksElement.addEventListener("click", function () {
+        event.preventDefault(); // Prevent the default link behavior (preventing page reload)
+        toggleVisibility("index_pg", "Home | 0125.dog");
+    });
+}
+
+//runs the Lootcrate page
+function setupToggleLootListener() {
+    const showLinksElement = document.getElementById("showLoot");
+
+    // Adding a click event listener to the "showLinks" element
+    showLinksElement.addEventListener("click", function () {
+        event.preventDefault(); // Prevent the default link behavior (preventing page reload)
+        toggleVisibility("loot_pg", "Lootcrates | 0125.dog");
+    });
+}
+
+// Adding an event listener to ensure the DOM content is loaded before setting up the click listener
+document.addEventListener("DOMContentLoaded", function () {
+    setupToggleLinksListener();
+    setupToggleIndexListener();
+    setupToggleLootListener();
+});
+
